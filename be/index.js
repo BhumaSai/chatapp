13fbc651544b2f3ab38c19eb9e5a6a5a9b555e63
Bhuma_Sai <bhumasairam123@gmail.com>
2023-07-31 18:48:27 +0530

    last

--- be/index.js
+++ be/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-const server = require('https').createServer(app)
+const server = require('http').createServer(app)
 const { Server } = require('socket.io');
 const io = new Server(server, { cors: { origin: 'https://feelfreetochat.netlify.app/' } })
 require('dotenv').config()
